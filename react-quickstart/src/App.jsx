0507e6735d3233ad0101eac67a8c3f6b43cc909d
unknown <@student.laverdad.edu.ph>
2024-10-25 22:51:32 +0800

    Im done completing the section on adding styles

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,21 +1,8 @@
-function AboutPage() {
-  return (
-    <>
-      <h1>About</h1>
-      <p>
-        Hello there.
-        <br />
-        How do you do?
-      </p>
-    </>
-  );
-}
-
 function App() {
   return (
     <>
       <h1>Welcome to my app</h1>
-      <AboutPage />
+      <img className="avatar" src="https://i.imgur.com/yXOvdOSs.jpg" />
     </>
   );
 }
