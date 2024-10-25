52f8c29ca53886b660955852bb248c697f81490d
unknown <@student.laverdad.edu.ph>
2024-10-26 00:26:06 +0800

    I'm done completing the section for updating the screen

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 function MyButton() {
+  const [count, setCount] = useState(0);
+
   function handleClick() {
-    alert("You clicked me!");
+    setCount(count + 1);
   }
 
-  return <button onClick={handleClick}>Click me</button>;
+  return <button onClick={handleClick}>Clicked {count} times</button>;
 }
+
 export default MyButton;
