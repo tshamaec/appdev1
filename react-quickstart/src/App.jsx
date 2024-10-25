d10bdcb9fa8a9c343f65e0146ce2bee34d3d952a
unknown <@student.laverdad.edu.ph>
2024-10-26 00:19:30 +0800

    I'm done completing the section on responding to events

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,24 +1,8 @@
-import "./App.css";
-import AdminPanel from "./AdminPanel";
-import LoginForm from "./LoginForm";
+function MyButton() {
+  function handleClick() {
+    alert("You clicked me!");
+  }
 
-const products = [
-  { title: "Cabbage", isFruit: false, id: 1 },
-  { title: "Garlic", isFruit: false, id: 2 },
-  { title: "Apple", isFruit: true, id: 3 },
-];
-
-export default function ShoppingList() {
-  const listItems = products.map((product) => (
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? "magenta" : "darkgreen",
-      }}
-    >
-      {product.title}
-    </li>
-  ));
-
-  return <ul>{listItems}</ul>;
+  return <button onClick={handleClick}>Click me</button>;
 }
+export default MyButton;
