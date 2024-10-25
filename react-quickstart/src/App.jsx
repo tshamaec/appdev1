408db4e5e87ab54b43da5bbd0accf603ab0ac8a4
unknown <@student.laverdad.edu.ph>
2024-10-25 23:20:56 +0800

    Im done completing the section on displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,8 +1,11 @@
+import "./App.css";
+import Profile from "./Profile";
+
 function App() {
   return (
     <>
       <h1>Welcome to my app</h1>
-      <img className="avatar" src="https://i.imgur.com/yXOvdOSs.jpg" />
+      <Profile />
     </>
   );
 }
